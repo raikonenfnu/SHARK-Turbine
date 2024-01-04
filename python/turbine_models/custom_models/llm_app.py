88c3520862ed7689c86710243ce2bf2ebca46826
stanley-nod <stanley@nod-labs.com>
2024-01-04 01:08:14 -0800

    Added evict in decode + general cleanup and refactor.

--- python/turbine_models/custom_models/llm_app.py
+++ python/turbine_models/custom_models/llm_app.py
@@ -40,7 +40,7 @@ parser.add_argument(
     help="local-sync, local-task, cuda, vulkan, rocm",
 )
 parser.add_argument(
-    "--init_cache",
+    "--streaming_llm",
     type=bool,
     default=False,
     help="Use KV-Cache in between user prompts/multi-dialogue.",
@@ -65,13 +65,13 @@ def append_user_prompt(history, input_prompt):
 
 
 def append_bot_prompt(history, input_prompt):
-    user_prompt = f"{B_SYS} {input_prompt} {E_SYS}"
+    user_prompt = f"{B_SYS} {input_prompt}{E_SYS} {E_SYS}"
     history += user_prompt
     return history
 
 
 class SharkLLM(object):
-    def __init__(self, device, vmfb_path, external_weight_path, init_cache=False):
+    def __init__(self, device, vmfb_path, external_weight_path, streaming_llm=False):
         self.runner = vmfbRunner(
             device=device,
             vmfb_path=vmfb_path,
@@ -80,7 +80,7 @@ class SharkLLM(object):
         self.first_input = True
         self.num_tokens = 0
         self.last_prompt = None
-        self.init_cache = init_cache
+        self.streaming_llm = streaming_llm
         self.prev_token_len = 0
 
     def format_out(self, results):
@@ -91,18 +91,21 @@ class SharkLLM(object):
 
     def generate(self, input_ids):
         # TODO: Replace with args.
-        if self.init_cache and self.runner.ctx.modules.state_update["get_seq_step"]() > 600:
+        if (
+            self.streaming_llm
+            and self.runner.ctx.modules.state_update["get_seq_step"]() > 600
+        ):
             print("Evicting cache space!")
             self.runner.ctx.modules.state_update["evict_kvcache_space"]()
         turbine_results = []
         # Only need not seen token for init cache
         # Because we have stored the res in KV-cache.
         token_len = input_ids.shape[-1]
-        if self.init_cache:
+        if self.streaming_llm:
             token_slice = max(self.prev_token_len - 1, 0)
             input_ids = input_ids[:, token_slice:]
         inputs = [ireert.asdevicearray(self.runner.config.device, input_ids)]
-        if self.first_input or not self.init_cache:
+        if self.first_input or not self.streaming_llm:
             s = time.time()
             results = self.runner.ctx.modules.state_update["run_initialize"](
                 *inputs
@@ -125,7 +128,10 @@ class SharkLLM(object):
             token_len += 1
         s = time.time()
         while self.format_out(results) != 2:
-            if self.init_cache and self.runner.ctx.modules.state_update["get_seq_step"]() > 600:
+            if (
+                self.streaming_llm
+                and self.runner.ctx.modules.state_update["get_seq_step"]() > 600
+            ):
                 print("Evicting cache space!")
                 self.runner.ctx.modules.state_update["evict_kvcache_space"]()
             results = self.runner.ctx.modules.state_update["run_forward"](results)
@@ -148,7 +154,7 @@ def run_llm(
     hf_model_name,
     hf_auth_token,
     external_weight_path,
-    init_cache,
+    streaming_llm,
 ):
     runner = vmfbRunner(
         device=device, vmfb_path=vmfb_path, external_weight_path=external_weight_path
@@ -162,7 +168,7 @@ def run_llm(
         device=device,
         vmfb_path=vmfb_path,
         external_weight_path=external_weight_path,
-        init_cache=init_cache,
+        streaming_llm=streaming_llm,
     )
     prompt = system_prompt
     while True:
@@ -171,7 +177,7 @@ def run_llm(
         initial_input = tokenizer(prompt, return_tensors="pt")
         example_input_id = initial_input.input_ids
         result = llm.generate(example_input_id)
-        bot_response = tokenizer.decode(result)
+        bot_response = tokenizer.decode(result, skip_special_tokens=True)
         print(f"\nBOT: {bot_response}\n")
         prompt = append_bot_prompt(prompt, bot_response)
 
@@ -186,5 +192,5 @@ if __name__ == "__main__":
         args.hf_model_name,
         args.hf_auth_token,
         args.external_weight_path,
-        args.init_cache,
+        args.streaming_llm,
     )
